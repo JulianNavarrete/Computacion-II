b21a4e775679c36fcf9806a6cd0c111cbacd388c
JulianNavarrete <julyplottier@hotmail.com>
2021-04-25 23:57:51 -0300

    ej_7 ready

---
+++ TPs/4. signal/ej_7.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import os
+import getopt
+from sys import argv
+import signal
+
+
+def handler(s, f):
+    print("Soy el PID ", os.getpid(), ", recibí la señal ", str(s), " de mi padre PID ", os.getppid())
+
+
+signal.signal(signal.SIGUSR2, handler)
+n = 0
+
+(opts, args) = getopt.getopt(argv[1:], 'p:', ['process'])
+
+for arg in opts:
+    if arg[0] == "-p":
+        n = arg[1]
+
+
+ppid = os.getppid()
+
+try:
+    if int(n) > 0:
+        for i in range(int(n)):
+            if ppid == os.getppid():
+                pid = os.fork()
+                if pid == 0:
+                    signal.pause()
+                else:
+                    print("Creando proceso:", str(pid))
+                    os.kill(pid, signal.SIGUSR2)
+
+    else:
+        print("Ingrese un valor mayor que 0 como parámtero por favor.")
+
+except:
+    print("Ingrese un valor mayor que 0 como parámtero por favor.")
