6c0fbee224245810a6f17692174e790cab6dd209
JulianNavarrete <julyplottier@hotmail.com>
2021-11-25 17:57:03 -0300

    TP Recuperatorio ready

--- TPs/TP Recuperatorio/main.py
+++ TPs/TP Recuperatorio/main.py
@@ -4,11 +4,8 @@ import numpy as np
 
 def rotarImagen(ubic):
     im = Image.open(ubic)
-    # im.show()
     alto_img = im.size[0]
     ancho_img = im.size[1]
-    im_copia = im
-    matriz_principal = np.asarray(im)
 
     matriz_roja_entrada = np.ones((alto_img, ancho_img))
     matriz_verde_entrada = np.empty((alto_img, ancho_img))
@@ -16,7 +13,8 @@ def rotarImagen(ubic):
     matriz_roja_salida = np.empty((ancho_img, alto_img))
     matriz_verde_salida = np.empty((ancho_img, alto_img))
     matriz_azul_salida = np.empty((ancho_img, alto_img))
-    matriz_final = np.empty((ancho_img, alto_img))
+
+    img_nueva = Image.new('RGB', (ancho_img, alto_img), color='black')
 
     for x in range(alto_img):
         for y in range(ancho_img):
@@ -43,25 +41,14 @@ def rotarImagen(ubic):
         for y in range(ancho_img):
             matriz_azul_salida[y, col_salida] = matriz_azul_entrada[x, y]
 
-    lista = []
     for x in range(ancho_img):
         for y in range(alto_img):
             pixel = tuple([int(matriz_roja_salida[x, y]), int(matriz_verde_salida[x, y]), int(matriz_azul_salida[x, y])])
-            # print(pixel)
-            # matriz_final[x, y] = pixel
+            img_nueva.putpixel((x, y), pixel)
 
-    img_final = Image.fromarray(np.uint8(ar))
-    Image.Image.show(img_final)
-
-    '''for x in range(ancho_img):
-        for y in range(alto_img):
-            pixel = tuple([int(matriz_roja_salida[x, y]), int(matriz_verde_salida[x, y]), int(matriz_azul_salida[x, y])])
-            im_copia.putpixel((x, y), pixel)
-            # print(x,y)
-    Image.Image.show(im_copia)'''
+    Image.Image.show(img_nueva)
 
 
 if __name__ == '__main__':
-    # ubic = input("Ingrese el nombre de la imagen con su respectiva extensión: ")
-    ubic = "yacht.ppm"
+    ubic = input("Ingrese el nombre de la imagen con su respectiva extensión: ")
     rotarImagen(ubic)
