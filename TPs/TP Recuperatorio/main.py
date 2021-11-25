147065f3cdfa085b28e4ae42e63107af9731b71b
JulianNavarrete <julyplottier@hotmail.com>
2021-11-25 15:51:57 -0300

    recuperatorio works only with squared images

--- TPs/TP Recuperatorio/main.py
+++ TPs/TP Recuperatorio/main.py
@@ -2,14 +2,12 @@ from PIL import Image
 import numpy as np
 
 
-def rotarImagen():
-    # ubic = str(input("Ingrese el nombre de la imagen con su respectiva extensión: "))
-    ubic = "yacht.ppm"
+def rotarImagen(ubic):
     im = Image.open(ubic)
+    # im.show()
     alto_img = im.size[0]
     ancho_img = im.size[1]
-    matriz_principal = np.asarray(im)
-    # print(matriz_principal)
+    im_copia = im
 
     matriz_roja_entrada = np.ones((alto_img, ancho_img))
     matriz_verde_entrada = np.empty((alto_img, ancho_img))
@@ -17,7 +15,6 @@ def rotarImagen():
     matriz_roja_salida = np.empty((ancho_img, alto_img))
     matriz_verde_salida = np.empty((ancho_img, alto_img))
     matriz_azul_salida = np.empty((ancho_img, alto_img))
-    matriz_final = np.empty((ancho_img, alto_img))
 
     for x in range(alto_img):
         for y in range(ancho_img):
@@ -44,18 +41,15 @@ def rotarImagen():
         for y in range(ancho_img):
             matriz_azul_salida[y, col_salida] = matriz_azul_entrada[x, y]
 
-    for x in range(alto_img):
-        for y in range(ancho_img):
-            matriz_final[x, y][] = (matriz_roja_salida[x, y][0], matriz_verde_salida[x, y][1], matriz_azul_salida[x, y][2])
+    for x in range(ancho_img):
+        for y in range(alto_img):
+            pixel = tuple([int(matriz_roja_salida[x, y]), int(matriz_verde_salida[x, y]), int(matriz_azul_salida[x, y])])
+            im_copia.putpixel((x, y), pixel)
 
-    img_final = Image.fromarray(np.uint8(matriz_final))
-    Image.Image.show(img_final)
-
-
-    # print(im)
-    # print(matriz_roja_entrada)
-    # print(matriz_azul_salida)
+    Image.Image.show(im_copia)
 
 
 if __name__ == '__main__':
-    rotarImagen()
+    # ubic = input("Ingrese el nombre de la imagen con su respectiva extensión: ")
+    ubic = "tux.ppm"
+    rotarImagen(ubic)
