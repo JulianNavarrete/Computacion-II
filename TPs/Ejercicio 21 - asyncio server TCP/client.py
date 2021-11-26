566577c28b770d5dcc86e7138dce1d6978b6b5d9
JulianNavarrete <julyplottier@hotmail.com>
2021-11-25 21:37:45 -0300

    ej-21 in progress

---
+++ TPs/Ejercicio 21 - asyncio server TCP/client.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import socket
+import sys
+import getopt
+from datetime import datetime as dt
+
+
+host = "localhost"
+port = 8000
+log_file = ""
+
+try:
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'l:')
+    for args in opt:
+        if args[0] == '-l':
+            log_file = args[1]
+except:
+    print("Error de parámetros.")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, int(port)))
+
+
+print("-----------Command Prompt-----------\n")
+while True:
+    msg = input("> ")
+    if msg != "exit":
+        s.send(msg.encode('utf-8'))
+        msg = s.recv(1024)
+        print(msg.decode('utf-8'))
+        if log_file != "":
+            with open(log_file, "a") as file:
+                td = dt.today()
+                file.writelines("\n" + str(td) + msg.decode('utf-8'))
+
+    else:
+        s.send("exit".encode('utf-8'))
+        s.close()
+        break
+
+print("\nCerrando conexión...")
---
+++ TPs/Ejercicio 21 - asyncio server TCP/server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+import asyncio
+import socket
+import subprocess
+
+
+async def function(cs, a):
+    while True:
+        msg = cs.recv(1024).decode('utf-8')
+        if msg != "exit":
+            res = subprocess.Popen([msg], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            std_out, std_err = res.communicate()
+            if std_out != "":
+                msg = "\nOK\n" + std_out
+            if std_err != "":
+                msg = "\nERROR\n" + std_err
+
+            cs.send(msg.encode('utf-8'))
+
+        else:
+            cs.close()
+            # s.close()
+            print("Conexión cerrada", a)
+            break
+
+
+async def main():
+    host = "localhost"
+    port = 8000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, int(port)))
+    s.listen(5)
+    print("Esperando conexiones...")
+    while True:
+        clientsocket, addr = s.accept()
+        print("Tengo una conexión de", str(addr))
+
+
+        '''
+        p = Process(target=child, args=(clientsocket, addr))
+        p.start()
+        '''
+
+
+asyncio.run(main())
